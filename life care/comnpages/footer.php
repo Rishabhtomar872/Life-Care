c3be9c72650c04167b1a5a2140f26073e9b3e31c
Rishabh Tomar <rishabhtomar872@gmail.com>
2022-04-30 12:24:23 +0530

    Initial commit

---
+++ life care/comnpages/footer.php
@@ -0,0 +1,68 @@
+<footer class="page_footer ds color section_padding_top_100 section_padding_bottom_65 table_section">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 col-sm-6">
+							<div class="widget widget_text"> <a href="./" class="logo vertical_logo bottommargin_20">
+	                    <img src="images/logo1.jpg" alt="" width="80%" float="left" style="margin-left: 0px">
+	                </a>
+								<p>Lorem ipsum dlor amet, consetetur sadipscing esed dia nonumy eirmod tempor invidunt.</p>
+								<p class="darklinks"> <a class="social-icon soc-facebook" href="#" title="Facebook"></a> <a class="social-icon soc-twitter" href="#" title="Twitter"></a> <a class="social-icon soc-google" href="#" title="Twitter"></a> <a class="social-icon soc-linkedin" href="#" title="Twitter"></a>									<a class="social-icon soc-youtube" href="#" title="Youtube"></a> </p>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="widget widget_text topmargin_25">
+								<h4 class="widget-title">Contact Us</h4>
+								<div class="media">
+									<div class="media-left rightpadding_10"> <i class="fa fa-map-marker highlight" aria-hidden="true"></i> </div>
+									<div class="media-body"> 482 LMN Street, PQR City, KS, 66223 </div>
+								</div>
+								<div class="media">
+									<div class="media-left rightpadding_10"> <i class="fa fa-phone highlight" aria-hidden="true"></i> </div>
+									<div class="media-body"> +91 8602768216 </div>
+								</div>
+								<div class="media">
+									<div class="media-left rightpadding_10"> <i class="fa fa-pencil highlight" aria-hidden="true"></i> </div>
+									<div class="media-body highlight2links"> <a href="mailto:pharmacom@example.com">mygmail@example.com</a> </div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109741.02912911311!2d76.69348873658222!3d30.73506264436677!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fed0be66ec96b%3A0xa5ff67f9527319fe!2sChandigarh!5e0!3m2!1sen!2sin!4v1593363347767!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+						</div>
+						
+					</div>
+				</div>
+			</footer>
+			
+			<link rel = "stylesheet" href="mystyle.css">
+<script src="myscript.js"></script>
+<button class="open-button" onclick="openForm()">Chat</button>
+
+<div class="chat-popup" id="myForm">
+  <form class="form-container">
+    
+
+    <label for="msg"><b>Chat Message</b></label>
+    <textarea placeholder="Type message.." name="msg" required></textarea>
+
+    <button type="submit" class="btn">Send</button>
+    <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+  </form>
+</div>
+			
+			
+			<section class="ds color darker page_copyright section_padding_15">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<p class="fontsize_12">&copy; Copyright <?php echo date("Y"); ?> All Rights Reserved.</p>
+							<p class="fontsize_12">Developed By : <a href="#" target="_blank">ABC and XYZ</a> </p>
+						</div>
+					</div>
+				</div>
+			</section>
+			
+			
+			
+			
+			
